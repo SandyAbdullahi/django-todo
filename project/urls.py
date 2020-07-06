395a125bc1981623e000b8a328e3e0e7d8a010d4
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-06 19:17:27 +0300

    add minor styling to about and landing

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from project.settings import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', include('project.todo.urls')),
     path('admin/', admin.site.urls),
 ]
+if DEBUG:
+    urlpatterns += static(STATIC_URL, document_root = STATIC_ROOT)
+    urlpatterns += static(MEDIA_URL, document_root = MEDIA_ROOT)
