82902aefa3dd1fe0bbcff289ad1c5f383764dc8d
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-06 19:54:51 +0300

    add minor styling to landing

--- project/todo/views.py
+++ project/todo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def LANDING(request):
-    return render(request, 'pages/landing.html')
+    count = User.objects.count()
+    context = {
+        'count': count
+    }
+    return render(request, 'pages/landing.html', context)
 
 def ABOUT(request):
+    return render(request, 'pages/about.html')
+
+
+def NEW_USER(request):
     return render(request, 'pages/about.html')
